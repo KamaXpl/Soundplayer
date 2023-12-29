4c95a5e579a66a2d27f2327df81e0e8fd990c2eb
KamaXpl <146933413+KamaXpl@users.noreply.github.com>
2023-12-29 19:00:38 +0100

    Add files via upload

--- sound.py
+++ sound.py
@@ -15,13 +15,13 @@ x = (screen_width - window_width) // 2
 y = (screen_height - window_height) // 2
 root.geometry('{}x{}+{}+{}'.format(window_width, window_height, x, y))
 
-def sign_out(): # funkcja wyloguj
+def sign_out():  # funkcja wyloguj
     answer = tkmb.askquestion(title='DebtyVibe',message='Czy na pewno chcesz sie wylogować?')
     if answer == "yes":
         root.destroy()
         from signin import sign_in
 
-def settings(): # funkcja ustawienia
+def settings():  # funkcja ustawienia
     settings_wind = ctk.CTkToplevel()
     settings_wind.geometry('300x300')
     settings_wind.mainloop()
@@ -30,20 +30,33 @@ def support():
     url = r'file:///C:\Users\PC\Documents\GitHub\Soundplayer\support.html'
     webbrowser.open_new_tab(url)
 
+def add():  
+    url = r'https://github.com/KamaXpl/Pingolingo'
+    webbrowser.open_new_tab(url)
+
 settings_frame = ctk.CTkFrame(master=root, width=100,height=1080, fg_color="#181818", border_color="#161616", border_width=7)   # ramka ustawien
 settings_frame.place(x=0,y=0) 
 
 song_list_frame = ctk.CTkFrame(master=root, width=900,height=650, fg_color="#181818", border_color="#161616", border_width=7)   # ramka playlisty
 song_list_frame.place(x=1020,y=0) 
 
+list_frame = ctk.CTkFrame(master=root, width=820,height=500, fg_color="#202020", border_color="#761A06", border_width=7)   # ramka playlisty
+list_frame.place(x=1060,y=100) 
+
 mixer_frame = ctk.CTkFrame(master=root, width=900,height=600, fg_color="#181818", border_color="#161616", border_width=7)   # ramka mikseru
 mixer_frame.place(x=1020,y=640)
+
+fav_song_list_frame = ctk.CTkFrame(master=root, width=820,height=320, fg_color="#202020", border_color="#761A06", border_width=7)   # ramka fav_playlisty
+fav_song_list_frame.place(x=1060,y=730) 
 
 panel_frame = ctk.CTkFrame(master=root, width=600,height=200, fg_color="#202020", border_color="#232323", border_width=7) # ramka panelu
 panel_frame.place(x=250,y=700)
 
 ad_frame = ctk.CTkFrame(master=root, width=800,height=600, fg_color="#202020", border_color="#232323", border_width=7)  # reklama
 ad_frame.place(x=160,y=50)
+
+ad_sec_frame = ctk.CTkFrame(master=root, width=410,height=410, fg_color="#202020", border_color="#761A06", border_width=7)  # reklama
+ad_sec_frame.place(x=355,y=95)
 
 def volumechanger():
     pygame.mixer.music.set_volume(volume_changer.get())
@@ -58,25 +71,26 @@ def pause():
 def unpause():
     pygame.mixer.music.unpause()
 
-volume_changer = ctk.CTkSlider(root, from_=0, to=100, command=volumechanger)
-volume_changer.place(x=450, y=870)
+volume_changer = ctk.CTkSlider(root, from_=0, to=100, button_color="#761A06", command=volumechanger)
+volume_changer.place(x=450, y=850)
 
-play_image = ctk.CTkImage(Image.open("playbtn.png"), size=(30,30))
+play_image = ctk.CTkImage(Image.open("playbtn.png"), size=(40,40))
 play_btn = ctk.CTkButton(master=root, text="", image=play_image, width=30, height=30, fg_color="#181818", command=play)
-play_btn.place(x=400, y=800)
+play_btn.place(x=492, y=780)
 
-pause_image = ctk.CTkImage(Image.open("playbtn.png"), size=(30,30))
-pause_btn = ctk.CTkButton(master=root, text="", image=play_image, width=30, height=30, fg_color="#181818", command=pause)
-pause_btn.place(x=450, y=800)
+prev_image = ctk.CTkImage(Image.open("prevbtn.png"), size=(40,40))
+prev_btn = ctk.CTkButton(master=root, text="", image=prev_image, width=30, height=30, fg_color="#181818", command=pause)
+prev_btn.place(x=432, y=780)
 
-unpause_image = ctk.CTkImage(Image.open("playbtn.png"), size=(30,30))
-unpause_btn = ctk.CTkButton(master=root, text="", image=play_image, width=30, height=30, fg_color="#181818", command=unpause)
-unpause_btn.place(x=500, y=800)
+next_image = ctk.CTkImage(Image.open("nextbtn.png"), size=(40,40))
+next_btn = ctk.CTkButton(master=root, text="", image=next_image, width=30, height=30, fg_color="#181818", command=unpause)
+next_btn.place(x=552, y=780)
 
-playlist_label = ctk.CTkLabel(master=root, text="Playlist", fg_color="#181818", text_color="#720404", font=("Impact", 48))
-playlist_label.place(x=1030,y=5)
+loop_image = ctk.CTkImage(Image.open("loopbtn.png"), size=(40,40))
+loop_btn = ctk.CTkButton(master=root, text="", image=loop_image, width=30, height=30, fg_color="#181818", command=unpause)
+loop_btn.place(x=612, y=780)
 
-logo_image = ctk.CTkImage(Image.open("logo.jpg"), size=(90, 90)) # do podmiany zdjecie i sciezka
+logo_image = ctk.CTkImage(Image.open("logo.png"), size=(90, 90)) # do podmiany zdjecie i sciezka
 logo_button = ctk.CTkLabel(master=root, text="", image=logo_image)
 logo_button.place(x=0,y=0)
 
@@ -88,10 +102,63 @@ settings_image = ctk.CTkImage(Image.open("sett.png"), size=(80, 80)) # do podmia
 settings_button = ctk.CTkButton(master=root, text="", image=settings_image, fg_color="#181818", width=80, height=80, command=settings)
 settings_button.place(x=0,y=920)
 
-support_image = ctk.CTkImage(Image.open("supp.png"), size=(80, 80)) # do podmiany zdjecie i sciezka
+support_image = ctk.CTkImage(Image.open("support.png"), size=(80, 80)) # do podmiany zdjecie i sciezka
 support_button = ctk.CTkButton(master=root, text="", image=support_image, fg_color="#181818", width=80, height=80, command=support)
 support_button.place(x=0,y=760)
 
 
+
+
+# LABEL
+
+playlist_label = ctk.CTkLabel(master=root, 
+                              text="Playlist", 
+                              fg_color="#181818", 
+                              text_color="#720404", 
+                              font=("Impact", 48))
+
+playlist_label.place(x=1030,y=5)
+
+
+fav_song_label = ctk.CTkLabel(master=root, 
+                              text="Favorite songs", 
+                              fg_color="#181818", 
+                              text_color="#720404", 
+                              font=("Impact", 48))
+
+fav_song_label.place(x=1030,y=650)
+
+# REKLAMA
+
+tlo_image = ctk.CTkImage(Image.open("tlo.png"), size=(400,400))   # reklama pingolingo
+tlo_btn = ctk.CTkLabel(master=root, 
+                       text="", 
+                       image=tlo_image, 
+                       width=30, 
+                       height=30, 
+                       fg_color="#181818")
+
+tlo_btn.place(x=360,y=100)
+
+
+click_me_btn = ctk.CTkButton(master=root,
+                               text="Click me",
+                               width=100,
+                               height=60,
+                               font=("Impact", 20),
+                               fg_color=("white"),
+                               text_color=("#720404"),
+                               border_color=("white"),
+                               command=add)
+
+click_me_btn.place(x=515,y=400)
+
+pingolingo_label = ctk.CTkLabel(master=root, 
+                                text="Pingolingo",
+                                font=("Impact", 48),
+                                fg_color=("white"),
+                                text_color=("#720404"),)
+
+pingolingo_label.place(x=455,y=100)
 
 root.mainloop()
