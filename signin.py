d286720e72bdcfea81d7d60a49144f41ded6b429
KamaXpl <146933413+KamaXpl@users.noreply.github.com>
2023-12-30 19:20:49 +0100

    Add files via upload

--- signin.py
+++ signin.py
@@ -1,64 +1,82 @@
 from modules import *
 from PIL import ImageTk,Image
+import subprocess  # Import the subprocess module for opening another script
+import sys
 
-ctk.set_appearance_mode("dark") 
-ctk.set_default_color_theme("dark-blue") 
-  
-sign_in = ctk.CTk() 
+ctk.set_appearance_mode("dark")
+ctk.set_default_color_theme("dark-blue")
+
+sign_in = ctk.CTk()
 sign_in.iconbitmap('icon.ico')
-sign_in.title("DebtyVibe") 
+sign_in.title("DebtyVibe")
 window_width = 500
 window_height = 500
-screen_width = sign_in.winfo_screenwidth() 	# ustawianie okna logowania
+screen_width = sign_in.winfo_screenwidth()
 screen_height = sign_in.winfo_screenheight()
 x = (screen_width - window_width) // 2
 y = (screen_height - window_height) // 4
 sign_in.geometry('{}x{}+{}+{}'.format(window_width, window_height, x, y))
-  
-def sign_in_def(): 
-  
-    username = "1"
-    password = "1"
 
-    if user_entry.get() == username and user_pass.get() == password: 
+def sign_in_def():
+    # Get username and password from entry widgets
+    username = user_entry.get()
+    password = user_pass.get()
+
+    # Connect to the MySQL database
+    db = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="",
+        database="debtyvibe_database"
+    )
+
+    # Create a cursor object to interact with the database
+    cursor = db.cursor()
+
+    # Check if the username and password match a record in the database
+    cursor.execute("SELECT * FROM users WHERE username = %s AND password = %s", (username, password))
+    user = cursor.fetchone()
+
+    if user:
+        tkmb.showinfo(title="DebtyVibe", message="Login successful!")
         sign_in.destroy()
         from sound import root
-    elif user_entry.get() == username and user_pass.get() != password: 
-        tkmb.showwarning(title='Wrong password',message='Please check your password') 
-    elif user_entry.get() != username and user_pass.get() == password: 
-        tkmb.showwarning(title='Wrong username',message='Please check your username') 
-    else: 
-        tkmb.showerror(title="Login Failed",message="Invalid Username and password") 
+    else:
+        tkmb.showerror(title="Login Failed", message="Invalid Username and password")
+
+    # Close the database connection
+    cursor.close()
+    db.close()
 
 def create_account():
     sign_in.destroy()
     from signup import sign_up
 
-welcome_label = ctk.CTkLabel(sign_in,text="Witaj w DebtyVibe!") 
-welcome_label.pack(pady=20) 
-  
-login_frame = ctk.CTkFrame(master=sign_in) 
-login_frame.pack(pady=20,padx=40,fill='both',expand=True) 
-  
-signin_label = ctk.CTkLabel(master=login_frame,text='Zaloguj się!') 
-signin_label.pack(pady=12,padx=10) 
-  
-user_entry= ctk.CTkEntry(master=login_frame,placeholder_text="Username") 
-user_entry.pack(pady=12,padx=10) 
-  
-user_pass= ctk.CTkEntry(master=login_frame,placeholder_text="Password",show="*") 
-user_pass.pack(pady=12,padx=10) 
-  
-login_btn = ctk.CTkButton(master=login_frame,text='Login',command=sign_in_def) 
-login_btn.pack(pady=12,padx=10) 
-  
-remember_me = ctk.CTkCheckBox(master=login_frame,text='Remember Me') 
-remember_me.pack(pady=12,padx=10) 
+welcome_label = ctk.CTkLabel(sign_in, text="Witaj w DebtyVibe!")
+welcome_label.pack(pady=20)
+
+login_frame = ctk.CTkFrame(master=sign_in)
+login_frame.pack(pady=20, padx=40, fill='both', expand=True)
+
+signin_label = ctk.CTkLabel(master=login_frame, text='Zaloguj się!')
+signin_label.pack(pady=12, padx=10)
+
+user_entry = ctk.CTkEntry(master=login_frame, placeholder_text="Username")
+user_entry.pack(pady=12, padx=10)
+
+user_pass = ctk.CTkEntry(master=login_frame, placeholder_text="Password", show="*")
+user_pass.pack(pady=12, padx=10)
+
+login_btn = ctk.CTkButton(master=login_frame, text='Login', command=sign_in_def)
+login_btn.pack(pady=12, padx=10)
+
+remember_me = ctk.CTkCheckBox(master=login_frame, text='Remember Me')
+remember_me.pack(pady=12, padx=10)
 
 label2 = ctk.CTkLabel(master=login_frame, text="Nie masz konta?")
-label2.pack(pady=12,padx=10)
+label2.pack(pady=12, padx=10)
 
 btn_signup = ctk.CTkButton(master=login_frame, text="Stwórz je tutaj!", command=create_account)
 btn_signup.pack(pady=12, padx=10)
-  
+
 sign_in.mainloop()
--- signup.py
+++ signup.py
@@ -1,25 +1,26 @@
 from modules import *
+import mysql.connector
 
-ctk.set_appearance_mode("dark") 
-ctk.set_default_color_theme("dark-blue") 
-  
-sign_up = ctk.CTk() 
+ctk.set_appearance_mode("dark")
+ctk.set_default_color_theme("dark-blue")
+
+sign_up = ctk.CTk()
 sign_up.iconbitmap('icon.ico')
-sign_up.title("DebtyVibe") 
+sign_up.title("DebtyVibe")
 window_width = 400
 window_height = 400
-screen_width = sign_up.winfo_screenwidth() 	# ustawianie okna logowania
+screen_width = sign_up.winfo_screenwidth()
 screen_height = sign_up.winfo_screenheight()
 x = (screen_width - window_width) // 2
 y = (screen_height - window_height) // 4
 sign_up.geometry('{}x{}+{}+{}'.format(window_width, window_height, x, y))
-  
-def sign_up_def(): 
-    us = "user"
-    pas = "passwords"
 
-    user_entry.get()
-    user_pass.get()
+# Function to sign up the user
+def sign_up_def():
+    # Get username and password from entry widgets
+    username = user_entry.get()
+    password = user_pass.get()
+
     if len(user_entry.get()) >= 3 and len(user_pass.get()) >= 7: 
         tkmb.showinfo(title="DebtyVibe", message="Utworzono konto! \n Teraz możesz sie zalogować do DebtyVibe!")
         sign_up.destroy()
@@ -30,23 +31,52 @@ def sign_up_def():
         tkmb.showwarning(title='DebtyVibe',message='Niepoprawna nazwa!') 
     else: 
         tkmb.showerror(title="DebtyVibe",message="Niepoprawna nazwa i hasło!") 
+    
+    # Connect to the MySQL database
+    db = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="",
+        database="debtyvibe_database"
+    )
 
-welcome_label = ctk.CTkLabel(sign_up,text="Witaj w DebtyVibe!") 
-welcome_label.pack(pady=20) 
-  
-login_frame = ctk.CTkFrame(master=sign_up) 
-login_frame.pack(pady=20,padx=40,fill='both',expand=True) 
-  
-signin_label = ctk.CTkLabel(master=login_frame,text='Tutaj stworzysz konto!') 
-signin_label.pack(pady=12,padx=10) 
-  
-user_entry= ctk.CTkEntry(master=login_frame,placeholder_text="Username") 
-user_entry.pack(pady=12,padx=10) 
-  
-user_pass= ctk.CTkEntry(master=login_frame,placeholder_text="Password",show="*") 
-user_pass.pack(pady=12,padx=10) 
-  
-login_btn = ctk.CTkButton(master=login_frame,text='Stwórz konto',command=sign_up_def) 
-login_btn.pack(pady=12,padx=10) 
-  
+    # Create a cursor object to interact with the database
+    cursor = db.cursor()
+
+    # Check if the username already exists
+    cursor.execute("SELECT * FROM users WHERE username = %s", (username,))
+    existing_user = cursor.fetchone()
+
+    if existing_user:
+        tkmb.showwarning(title='DebtyVibe', message='Username already exists!')
+    else:
+        # Insert the new user into the database
+        cursor.execute("INSERT INTO users (username, password) VALUES (%s, %s)", (username, password))
+        db.commit()
+        tkmb.showinfo(title="DebtyVibe", message="Account created successfully! You can now log in to DebtyVibe.")
+        sign_up.destroy()
+        from signin import sign_in
+
+    # Close the database connection
+    cursor.close()
+    db.close()
+
+welcome_label = ctk.CTkLabel(sign_up, text="Welcome to DebtyVibe!")
+welcome_label.pack(pady=20)
+
+login_frame = ctk.CTkFrame(master=sign_up)
+login_frame.pack(pady=20, padx=40, fill='both', expand=True)
+
+signin_label = ctk.CTkLabel(master=login_frame, text='Create your account here!')
+signin_label.pack(pady=12, padx=10)
+
+user_entry = ctk.CTkEntry(master=login_frame, placeholder_text="Username")
+user_entry.pack(pady=12, padx=10)
+
+user_pass = ctk.CTkEntry(master=login_frame, placeholder_text="Password", show="*")
+user_pass.pack(pady=12, padx=10)
+
+login_btn = ctk.CTkButton(master=login_frame, text='Create Account', command=sign_up_def)
+login_btn.pack(pady=12, padx=10)
+
 sign_up.mainloop()
